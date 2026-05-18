72e2e5f613323685b2e8ff87e621db06212d01d1
james <jamesvincent3bacus@gmail.com>
2026-05-18 17:01:47 +0800

    fastapi test

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from webhook-router!")
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+async def read_root():
+    return {"Hello":"World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
+
