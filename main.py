d4b20ec8928b46528f93bdc45e7efb21af0ab441
james <jamesvincent3bacus@gmail.com>
2026-06-05 09:40:35 +0800

    endpoint test

--- main.py
+++ main.py
@@ -8,6 +8,23 @@ async def read_root():
     return {"Hello":"World"}
 
 
+
+nameList = {"first":'james', 
+            "second":'john',
+            "third": 'doe'}
+
+@app.get("/test")
+async def test():
+    return nameList
+
+@app.post("/test/try/add")
+async def add_name(data : dict):
+    nameList.update(data)
+    return nameList
+
+
+
+
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
