4c1d2da6e348503c04eed839f5ad3288b08286c4
james <jamesvincent3bacus@gmail.com>
2026-06-08 18:02:34 +0800

    post test with db but connection issue

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 import psycopg2
 import os
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 
@@ -14,9 +15,13 @@ app = FastAPI()
 @app.post("/test/try/add")
 async def add_name(data : dict):
     cursor = db.cursor()
+    cursor.execute(
+        'INSERT INTO events (source, event_type, payload) VALUES (%s, %s, %s)', (data['source']), data['event_type'], json.dumps(data['payload'])
+        )
+    db.commit()
+    cursor.close()
+    return {"status": "received"}
 
-    cursor.execute()
-    return nameList
 
 
 @app.get("/")
