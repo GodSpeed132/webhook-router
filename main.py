093718ffe704591c90767d83c7428e5f7b04d4c2
james <jamesvincent3bacus@gmail.com>
2026-06-20 22:59:31 +0800

    test code cleanup

--- main.py
+++ main.py
@@ -13,32 +13,6 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 db = psycopg2.connect(DATABASE_URL, sslmode ='require')
 app = FastAPI()
 
-class EventType(str, Enum):
-    push = 'push'
-    get = 'get'
-    insert = 'insert'
-    update = 'update'
-    delete = 'delete'
-
-class WebhookEvent(BaseModel):
-    source: str
-    event_type: EventType
-    payload: dict
-
-
-
-@app.post("/test/try/add")
-async def test(data: WebhookEvent):
-    cursor = db.cursor()
-    cursor.execute(
-        'INSERT INTO events (source, event_type, payload) VALUES (%s, %s, %s)', 
-        (data.source, data.event_type.value, json.dumps(data.payload))
-        )
-    db.commit()
-    cursor.close()
-    return {"status": "received"}
-
-
 
 @app.post("/webhook/github")
 async def github(request: Request):
